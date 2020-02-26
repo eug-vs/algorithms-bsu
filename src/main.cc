cb92630d61a2738e4261fa3c4c91dbc7cc4b5561
Eug-VS <eug-vs@keemail.me>
2020-02-26 23:01:36 +0300

    feat: implement left- & rightmost search

--- src/main.cc
+++ src/main.cc
@@ -8,6 +8,37 @@ using namespace std;
 #define OUTPUT "output.txt"
 
 
+int leftmost(vector<int> v, int target) {
+  int L = 0;
+  int R = v.size();
+  int i;
+  while (L < R) {
+    i = (int)((L+R)/2);
+    if (v[i] < target) {
+      L = i + 1;
+    } else {
+      R = i;
+    }
+  }
+  return L;
+}
+
+int rightmost(vector<int> v, int target) {
+  int L = 0;
+  int R = v.size();
+  int i;
+  while (L < R) {
+    i = (int)((L+R)/2);
+    if (v[i] > target) {
+      R = i;
+    } else {
+      L = i + 1;
+    }
+  }
+  return R - 1;
+}
+
+
 int main() {
   ifstream fin;
   ofstream fout;
@@ -30,6 +61,13 @@ int main() {
   for (int i = 0; i< queries_length; i++) {
     fin >> queries[i];
   }
+
+  for (int query : queries){
+    int l = leftmost(array, query);
+    int r = rightmost(array, query) + 1;
+    bool b = (array[l] == query);
+    cout << b << " " << l << " " << r << endl;
+  };
 
   fout.close();
   fin.close();
