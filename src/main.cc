80691ae5db29ddea5d4780ca534a0199c54d77b5
Eug-VS <eug-vs@keemail.me>
2020-02-27 02:09:49 +0300

    feat: move Node class into Tree, add remove()

--- src/main.cc
+++ src/main.cc
@@ -5,50 +5,49 @@ using namespace std;
 #define INPUT "input.txt"
 #define OUTPUT "output.txt"
 
-class Node {
-  private:
-    Node* left;
-    Node* right;
 
-  public:
-    int value;
-
-    Node(int x) {
-      left = nullptr;
-      right = nullptr;
-      value = x;
-    }
-
-    void insert(int x) {
-      if (x < value) {
-        if (left) left->insert(x);
-        else left = new Node(x);
-      } else if (x > value) {
-        if (right) right->insert(x);
-        else right = new Node(x);
-      }
-    }
-
-    friend ostream& operator<< (ostream& os, const Node& node) {
-      os << node.value << endl;
-      if (node.left) os << *node.left;
-      if (node.right) os << *node.right;
-      return os;
-    }
-
-    ~Node() {
-      delete left;
-      delete right;
-    }
-};
 
 
 class Tree {
   private:
+    class Node {
+      public:
+        int value;
+        Node* left;
+        Node* right;
+
+        Node(int x) {
+          left = nullptr;
+          right = nullptr;
+          value = x;
+        }
+
+        void insert(int x) {
+          if (x < value) {
+            if (left) left->insert(x);
+            else left = new Node(x);
+          } else if (x > value) {
+            if (right) right->insert(x);
+            else right = new Node(x);
+          }
+        }
+
+        friend ostream& operator<< (ostream& os, const Node& node) {
+          os << node.value << endl;
+          if (node.left) os << *node.left;
+          if (node.right) os << *node.right;
+          return os;
+        }
+
+        ~Node() {
+          delete left;
+          delete right;
+        }
+    };
+
     Node* root;
 
   public:
-
     Tree() {
       root = nullptr;
     }
@@ -59,6 +58,40 @@ class Tree {
       } else {
         root = new Node(x);
       }
+    }
+
+    void remove(int x) {
+      Node** current = &root;
+      Node* child = nullptr;
+
+      while((child = *current) && (child->value !=x)) {
+        if (child->value > x) {
+          if (child->left) current = &child->left;
+          else return;
+        } else {
+          if (child->right) current = &child->right;
+          else return;
+        }
+      }
+
+      if (child->left && child->right) {
+        Node* min = child->right;
+        while(min->left) min = min->left;
+        int min_key = min->value;
+        remove(min_key);
+        child->value = min_key;
+        return;
+
+      } else if (child->left && !child->right) {
+        *current = child->left;
+        child->left = nullptr;
+      } else if (!child->left && child->right) {
+        *current = child->right;
+        child->right = nullptr;
+      } else {
+        *current = nullptr;
+      }
+      delete child;
     }
 
     friend ostream& operator<< (ostream& os, const Tree& tree) {
@@ -88,7 +121,8 @@ int main() {
     tree.insert(key);
   }
 
-  cout << tree;
+  tree.remove(target);
+  fout << tree;
 
   fout.close();
   fin.close();
